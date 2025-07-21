1bebddf1a395c1cc7b682d5beb7356fea5583c13
Your000 <bobo@example.com>
2025-07-21 17:00:24 +0800

    提交信息

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,29 +1,41 @@
 #include "hzpch.h"
 #include "Application.h"
 
-#include"Events/ApplicationEvent.h"
+
 #include"Log.h"
 #include <GLFW/glfw3.h>
 
 namespace Hazel {
-	//为Appliction类的构造函数， 创造一个WindowsWindow类型的对象,把地址值以多态（父类）的方式返回给m_Window
+#define BIND_EVENT_FN(x) std::bind(&Application::x,this,std::placeholders::_1)
 	Application::Application() {
 		m_Window =std::unique_ptr<Window>( Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application() {
 	}
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		HZ_CORE_INFO("{0}",e.ToString());
+	}
+
+	
 	void Application::Run()	
 	{
-		
-		
-		
-		
+	
 		while (m_Running) {
 			glClearColor(1, 0, 1, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
-			m_Window->OnUpdate();//调用glfwPollEvents()和glfwSwapBuffers(m_Window);
+			m_Window->OnUpdate();
 			
 		}
+	}
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+
+		m_Running = false;
+		return true;
 	}
 }
